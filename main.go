fb48e705b67f60bbb5d25889cde3a29403d2279c
Kiril Vladimirov <kiril@vladimiroff.org>
2013-11-21 19:47:53 +0200

    Install sockjs

---
+++ main.go
@@ -0,0 +1,10 @@
+
+package main
+
+import (
+	"github.com/Vladimiroff/mazungumzo/server"
+)
+
+func main() {
+	server.Start()
+}
---
+++ server/server.go
@@ -0,0 +1,47 @@
+// chat room example
+package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/fzzy/sockjs-go/sockjs"
+)
+
+var users *sockjs.SessionPool = sockjs.NewSessionPool()
+
+func chatHandler(s sockjs.Session) {
+	users.Add(s)
+	defer users.Remove(s)
+
+	for {
+		m := s.Receive()
+		if m == nil {
+			break
+		}
+		fullAddr := s.Info().RemoteAddr
+		addr := fullAddr[:strings.LastIndex(fullAddr, ":")]
+		m = []byte(fmt.Sprintf("%s: %s", addr, m))
+		users.Broadcast(m)
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./static/index.html")
+}
+
+func Start() {
+	mux := sockjs.NewServeMux(http.DefaultServeMux)
+	conf := sockjs.NewConfig()
+	http.Handle("/static", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/", indexHandler)
+	mux.Handle("/chat", chatHandler, conf)
+
+	log.Println("The server is up an running at http://0.0.0.0:8081")
+	err := http.ListenAndServe("0.0.0.0:8081", mux)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
