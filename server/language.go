60065fd5e5a00a964a7d7261ffb488485ec77ef9
Kiril Vladimirov <kiril@vladimiroff.org>
2013-11-26 20:07:48 +0200

    Implement sending to and streaming from each language

--- server/language.go
+++ server/language.go
@@ -1,7 +1,9 @@
 package server
 
 import (
+	"fmt"
 	"sync"
+	"time"
 
 	"github.com/Vladimiroff/mazungumzo/workq"
 )
@@ -9,15 +11,30 @@ import (
 type Language struct {
 	mutex   sync.RWMutex
 	clients map[string]*Client
-	queue   workq.Queue
+	queue   *workq.Queue
 	Name    string
 }
 
 func NewLanguage(name string) *Language {
 	l := new(Language)
 	l.clients = make(map[string]*Client)
+	l.queue = workq.NewQueue()
 	l.Name = name
+	go l.Stream()
 	return l
+}
+
+func (l *Language) Stream() {
+	for item := range l.queue.Stream() {
+		for _, client := range l.clients {
+			message := fmt.Sprintf("[%v]%s: %s",
+				time.Now().Format("15:04:05"), // TODO: Track time on send
+				item.Sender,
+				item.Translated,
+			)
+			client.Send(message)
+		}
+	}
 }
 
 func (l *Language) AddClient(c *Client) {
@@ -32,4 +49,17 @@ func (l *Language) RemoveClient(c *Client) {
 	defer l.mutex.Unlock()
 
 	delete(l.clients, c.Name)
+}
+
+func (l *Language) Send(sender *Client, message []byte) {
+	translatable := new(workq.Item)
+	*translatable = workq.Item{
+		Sender:  sender.Name,
+		Message: string(message),
+		Src:     sender.NativeLanguage,
+		Dest:    l.Name,
+		Done:    make(chan bool),
+	}
+
+	l.queue.Push(translatable)
 }
