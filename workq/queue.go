499e64eac56f7c6563ea3ca028c0d19d9f9718e8
Kiril Vladimirov <kiril@vladimiroff.org>
2013-11-26 20:11:01 +0200

    Use a channel inside the Queue

--- workq/queue.go
+++ workq/queue.go
@@ -1,37 +1,27 @@
 package workq
 
-import "sync"
-
 type Queue struct {
-	items []*Item
-	mutex sync.Mutex
+	ch chan *Item
+}
+
+func NewQueue() *Queue {
+	q := new(Queue)
+	q.ch = make(chan *Item, 10)
+	return q
 }
 
 func (q *Queue) Push(item *Item) {
-	q.mutex.Lock()
-	defer q.mutex.Unlock()
-
-	q.items = append(q.items, item)
+	q.ch <- item
 	go item.Translate()
 }
 
-func (q *Queue) Pop() *Item {
-	q.mutex.Lock()
-	defer q.mutex.Unlock()
-
-	if !q.IsEmpty() {
-		item := (q.items)[0]
-		<-item.Done
-		q.items = q.items[1:len(q.items)]
-		return item
-	}
-	return nil
-}
-
-func (q *Queue) Len() int {
-	return len(q.items)
-}
-
-func (q *Queue) IsEmpty() bool {
-	return q.Len() == 0
+func (q *Queue) Pop() <-chan *Item {
+	ch := make(chan *Item)
+	go func() {
+		for item := range q.ch {
+			<-item.Done
+			ch <- item
+		}
+	}()
+	return ch
 }
